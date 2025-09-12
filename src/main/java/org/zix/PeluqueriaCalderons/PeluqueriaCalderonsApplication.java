a06d0e886d3f2b58d5ddcae9f7dbdeaf25227009
SuarezGil <isuarez-2021691@kinal.edu.gt>
2025-09-12 15:58:28 -0600

    Se subio el proyecto

---
+++ src/main/java/org/zix/PeluqueriaCalderons/PeluqueriaCalderonsApplication.java
@@ -0,0 +1,13 @@
+package org.zix.PeluqueriaCalderons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeluqueriaCalderonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeluqueriaCalderonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zix/PeluqueriaCalderons/PeluqueriaCalderonsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zix.PeluqueriaCalderons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeluqueriaCalderonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
