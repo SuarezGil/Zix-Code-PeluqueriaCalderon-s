f6a3914a18d7b8d1c00115639f1257f652a9eaf0
Jeferson <jyaxon-2024442@kinal.edu.gt>
2025-09-26 12:01:24 -0600

    Creacion de Vistas

---
+++ src/main/java/org/zix/PeluqueriaCalderons/PeluqueriaCalderonsApplicationWeb.java
@@ -0,0 +1,12 @@
+package org.zix.PeluqueriaCalderons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeluqueriaCalderonsApplicationWeb {
+    public static void main (String[] args) {
+    SpringApplication.run(PeluqueriaCalderonsApplicationWeb.class);
+
+    }
+}
